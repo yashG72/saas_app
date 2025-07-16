3a7ac69e7f87c04557bd298a95984ca278a0cd9c
yashG72 <yashgoyal2874@gmail.com>
2025-07-16 17:40:10 +0530

    chore: update dependencies and add new packages
    - Added @hookform/resolvers, @radix-ui/react-label, and @radix-ui/react-select to dependencies.
    - Updated react-hook-form to version 7.60.0.
    - Added zod to dependencies.
    - Updated package-lock.json to reflect the changes in dependencies and their versions.

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,10 +1,15 @@
+import CompanionForm from '@/components/CompanionForm'
 import React from 'react'
 
 const NewCompanion = () => {
   return (
-    <div>
-      NewCompanion
-    </div>
+    <main className='min-lg:w-1/3 min-md:w-2/3 items-center justify-center'>
+      <article className='w-full gap-4 flex flex-col'>
+        <h1>Companion Form</h1>
+
+        <CompanionForm />
+      </article>
+    </main>
   )
 }
 
