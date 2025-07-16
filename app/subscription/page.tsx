6cccd60ffbcaf89738b7f03de5be852576a27c76
yashG72 <yashgoyal2874@gmail.com>
2025-07-16 18:54:55 +0530

    feat: replace placeholder text with PricingTable component in Subscription page

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,9 +1,10 @@
+import { PricingTable } from '@clerk/nextjs'
 import React from 'react'
 
 const Subscription = () => {
   return (
     <div>
-      Subscription
+      <PricingTable />
     </div>
   )
 }
