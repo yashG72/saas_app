fc6f5e4f8b8c33a5d30d0166c615b9d33f129933
yashG72 <yashgoyal2874@gmail.com>
2025-07-16 21:49:00 +0530

    feat: implement companion creation with authentication and redirect logic

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,7 +1,13 @@
 import CompanionForm from '@/components/CompanionForm'
+import { auth } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation';
 import React from 'react'
 
-const NewCompanion = () => {
+const NewCompanion = async () => {
+
+    const { userId } = await auth();
+    if(!userId) redirect('/sign-in');
+
   return (
     <main className='min-lg:w-1/3 min-md:w-2/3 items-center justify-center'>
       <article className='w-full gap-4 flex flex-col'>
